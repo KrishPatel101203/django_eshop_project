68e6d39b26f6b8b9d7cb2495ea8b9b77cab61c66
KrishPatel101203 <67914108+KrishPatel101203@users.noreply.github.com>
2020-07-07 11:29:41 +0530

    main e_shop files

--- urls.py
+++ urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from . import views
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
-    path('',views.category,name='home-page')
-    
+    path('',include('products.urls')),
+    path('',include('home_page.urls')),
+    path('admin/', admin.site.urls),
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
